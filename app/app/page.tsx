b90f4b59abad09fb0456cb3b7db0761c8fde9e2d
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-04-09 16:20:19 +0100

    correctons notification correction

---
+++ app/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+<section>
+    <main>
+        <div>
+            <section>
+                
+            </section>
+        </div>
+    </main>
+</section>  
+)
+}
+
+export default page
